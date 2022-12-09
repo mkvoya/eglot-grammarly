770a1470d248a3ffb3fd76ff7e65c71bb0a2db11
Mingkai Dong <mk@dong.mk>
2022-12-10 01:21:59 +0800

    add login support

--- eglot-grammarly.el
+++ eglot-grammarly.el
@@ -32,6 +32,7 @@
 ;;; Code:
 
 (require 'eglot)
+(require 'org-protocol)
 
 (defgroup eglot-grammarly nil
   "Settings for the Grammarly Language Server.
@@ -56,12 +57,56 @@ Link: https://github.com/znck/grammarly"
 (defclass eglot-grammarly-server (eglot-lsp-server) ()
   :documentation "A custom class for grammarly langserver.")
 
-(defconst eglot-grammarly-client-id "client_BaDkMgx4X19X9UxxYRCXZo"
+;; desktop
+(defconst eglot-grammarly-client-id "client_MAFL6NoqazFobEJb9SxMib"
   "Client ID is required for language server's activation.")
+;; web
+;; (defconst eglot-grammarly-client-id "client_2tUGvu1E2Dfu2tWjwgSHFd"
+;;    "Client ID is required for language server's activation.")
 
 (cl-defmethod eglot-initialization-options ((server eglot-grammarly-server))
   "Passes through required grammarly initialization options"
     (list :clientId eglot-grammarly-client-id))
+
+(defun eglot-grammarly-logout ()
+  "Logout the grammarly account."
+  (jsonrpc-request (eglot-current-server) :$/logout))
+
+(defun eglot-grammarly--login-ok (server)
+  "Whether user account is connected in SERVER."
+  (jsonrpc-request server :$/isUserAccountConnected nil))
+
+(defun eglot-grammarly-login-ok ()
+  "Whether user account is connected."
+  (interactive)
+  (eglot-grammarly--login-ok (eglot-current-server)))
+
+(defun eglot-grammarly-message ()
+    (message (if (eglot-grammarly-login-ok) "Login successfully!" "Login failed!")))
+
+(defvar eglot-grammarly--last-server nil "Last seen server")
+
+(defun eglot-grammarly-login ()
+  "Login to the grammarly account."
+  (interactive)
+  (let* ((window-id (format "%s" (emacs-pid))))
+    (setq eglot-grammarly--last-server (eglot-current-server))
+    (browse-url (jsonrpc-request eglot-grammarly--last-server :$/getOAuthUrl "org-protocol://eglot-grammarly/auth/callback"))))
+
+(defun eglot-grammarly-on-oauth-redirected (data)
+  "Handle oauth code in DATA sent from org-protocol."
+  (let* ((params (org-protocol-parse-parameters data nil '(:code :state)))
+         (code (plist-get params :code))
+         (state (plist-get params :state)))
+    (setq code (concat "org-protocol://?code=" code "&state=" state))  ; This is a non-documented magic...
+    (jsonrpc-request eglot-grammarly--last-server :$/handleOAuthCallbackUri code)
+    (message (if (eglot-grammarly--login-ok eglot-grammarly--last-server) "Login successfully!" "Login failed!")))
+  nil)
+
+(add-to-list 'org-protocol-protocol-alist
+             '("eglot-grammarly"
+               :protocol "eglot-grammarly/auth/callback"
+               :function eglot-grammarly-on-oauth-redirected))
 
 (provide 'eglot-grammarly)
 ;;; eglot-grammarly.el ends here
